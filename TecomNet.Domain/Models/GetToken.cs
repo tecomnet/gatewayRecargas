1f7f1b412ef7706455fd3ab1afc51f72181a9af5
tecomnet <>
2025-11-12 11:23:11 -0600

    modelo creado  GetToken

---
+++ TecomNet.Domain/Models/GetToken.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TecomNet.Domain.Models
+{
+    public class GetToken
+    {
+        [JsonPropertyName("accesToken")]
+        public string AccesToken { get; set; }
+        [JsonPropertyName("clientId")]
+        public string ClientId { get; set; }
+        [JsonPropertyName("tokenType")]
+        public string TokenType { get; set; }
+        [JsonPropertyName("issuedAt")]
+        public string IssuedAt { get; set; }
+        [JsonPropertyName("expiresIn")]
+        public string ExpiresIn { get; set; }
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+        [JsonPropertyName("scopes")]
+        public string Scopes { get; set; }
+
+        public int GetExpiresInSeconds()
+        {
+            if (int.TryParse(ExpiresIn, out var seconds))
+            {
+                return seconds;
+            }
+            return 0;
+        }
+    }
+}
